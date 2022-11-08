cd6bb0c7c729439ac75695050b875a3b3dab1cbb
Alex Fortuna <afortuna@oicr.on.ca>
2022-11-07 21:40:22 -0500

    update calculate

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,5 +6,5 @@ set -o pipefail
 cd $1
 
 find . -regex '.*\.PLASMA_VS_TUMOR_RESULT.csv' | grep -v _dis | xargs md5sum 
-find . -regex '.*\.PLASMA_VS_TUMOR_RESULT.csv.HBCs.txt' | grep -v _dis | xargs md5sum 
+find . -regex '.*\.HBCs.txt' | grep -v _dis | xargs md5sum 
 
