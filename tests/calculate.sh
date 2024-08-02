0044e6cb67138194a1a19c88be3588a3e9df2f9d
Gavin Peng <gpeng@oicr.on.ca>
2024-08-02 11:32:22 -0400

    change the test files

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 cd $1
 
-find -name SNP.count.txt | grep -v _dis | xargs md5sum 
+find -name *SNP.count.txt | grep -v _dis | xargs md5sum 
 find -name *.HBCs.csv | grep -v _dis | xargs md5sum 
 find -name *.mrdetect.vaf.txt | grep -v _dis | xargs md5sum
 find -name *.mrdetect.txt | grep -v _dis | xargs md5sum
